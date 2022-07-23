7317901367f6ee81563c94ce2cb417edb07a0688
Janes <james.webber3177@campus.mccd.edu>
2022-07-23 13:20:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-#sed -i 's/10.10.17.123/10.10.17.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.10.17.123/g' package/base-files/files/bin/config_generate
